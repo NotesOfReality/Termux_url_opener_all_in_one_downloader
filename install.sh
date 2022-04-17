dc9fe6a3eccb402528d96a6ebcc3d08bc4e2e7fe
NotesOfReality <michelecurcipio@gmail.com>
2022-04-17 19:07:07 +0200

    README : replace `wget` with `curl` in the installation command
    the former isn't pre-installed in termux.
    Unfortunately if I pipe`curl`output directly into `bash` the latter assumes I "take the default action" when it runs`pkg install ...` and the terminal prompts the user with multiples choices.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ rm -f "/data/data/com.termux/files/home/bin/termux-url-opener"
 mkdir /data/data/com.termux/files/home/bin
 
 #Installing dependencies
-pkg install python ffmpeg termux-api -y
+pkg update -y && pkg install wget python ffmpeg termux-api -y
 yes | pip install youtube-dl spotdl yt-dlp you-get rapidfuzz==1.9.1 beautifulsoup4==4.10.0
 
 wget --no-check-certificate --output-document="/data/data/com.termux/files/usr/bin/rbtv-dl" "https://raw.githubusercontent.com/M3GABOY/rbtv-dl/main/rbtv-dl.py"
